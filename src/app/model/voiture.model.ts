71a254482c7179a444a8340651ffcf2fef20ab25
FerchichiEya <101140478+FerchichiEya@users.noreply.github.com>
2022-12-11 19:21:12 +0100

    Add files via upload

---
+++ src/app/model/voiture.model.ts
@@ -0,0 +1,9 @@
+import { Categorie } from "./categorie.model";
+export class voitures {
+    idVoiture!: number;
+    nomVoiture! : string;
+    prixVoiture! : number;
+    dateCreation! : Date ;
+    categorie!:Categorie;
+    }
+    
